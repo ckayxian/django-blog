16a718a3e244625d48c44c2ef71d24b6f6748d81
ckayxian <ckayx15@gmail.com>
2022-06-09 16:05:51 -0700

    Template changes

--- blogging/tests.py
+++ blogging/tests.py
@@ -47,7 +47,7 @@ class FrontEndTestCase(TestCase):
     def test_list_only_publishing(self):
         resp = self.client.get("/")
         resp_text = resp.content.decode(resp.charset)
-        self.assertTrue("New Posts" in resp_text)
+        self.assertTrue("Most Recent Posts" in resp_text)
         for count in range(1, 11):
             title = "Post %d Title" % count
             if count < 6:
